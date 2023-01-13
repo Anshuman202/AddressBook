caa5dc3185dac2f6733f256f33b0a35323e11715
Anshuman202 <asanshuman2@gmail.com>
2023-01-13 19:50:35 +0530

    method calling

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
-﻿namespace AddressBook
+﻿namespace AddressBookProgram
 {
     public class Program
     {
-        public static void Main(String[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("******** Welcome to Address Book Program ********");
+            // Console.WriteLine("******** Welcome to Address Book Program ********");
 
+            AddressBook obj = new AddressBook();
+            obj.CreateUser();
+            obj.ShowAllPersonsInList();
         }
+
+
     }
 }
